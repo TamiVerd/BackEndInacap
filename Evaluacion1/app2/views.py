b793e7a8d091275de79f822a62f81e82d054ae3f
Tamimi <115664525+TamiVerd@users.noreply.github.com>
2025-09-08 22:17:57 -0400

    backend django

--- Evaluacion1/app2/views.py
+++ Evaluacion1/app2/views.py
@@ -1,3 +1,19 @@
+# app2/views.py
+
 from django.shortcuts import render
 
-# Create your views here.
+# Vista para la página de productos (todas las categorías)
+def productos(request):
+    return render(request, 'app2/productos.html')
+
+# Vista para la categoría de Electrónica
+def electronica(request):
+    return render(request, 'app2/electronica.html')
+
+# Vista para la categoría de Juguetes
+def juguetes(request):
+    return render(request, 'app2/juguetes.html')
+
+# Vista para la categoría de Ropa
+def ropa(request):
+    return render(request, 'app2/ropa.html')
